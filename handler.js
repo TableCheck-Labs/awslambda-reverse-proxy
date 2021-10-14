3f116f6dd7f079c43dc86d326e29b481238785b7
Alexander <4584443+DragonStuff@users.noreply.github.com>
2021-10-14 06:05:26 +0000

    feature(upgrade): remove tech debt

--- handler.js
+++ handler.js
@@ -3,6 +3,7 @@ const http = require('http')
 const url = require('url')
 
 var signRequest = (requestOptions) => { return requestOptions }
+
 if (process.env.AWS_SIGN_REQUESTS) {
   signRequest = require('aws4').sign
 }
@@ -13,25 +14,28 @@ const httpAgent = http.Agent({
 })
 
 module.exports.proxy = (event, context, callback) => {
-  var res = http.request(signRequest({
-    hostname: process.env.URL,
-    method: event.httpMethod,
-    headers: event.headers,
-    path: event.path,
-    agent: httpAgent,
-    timeout: 30 /* maximum time for api gateway invoked Lambda */
-  }))
+  var res = http.request(
+    signRequest({
+      hostname: process.env.URL,
+      method: event.httpMethod,
+      headers: event.headers,
+      path: event.path,
+      agent: httpAgent,
+      timeout: 30 /* maximum time for api gateway invoked Lambda */
+    })
+  )
 
   var resBody = ""
+  
   res.on('data', (chunk) => {
     resBody += chunk
-  })
-  .on('end', () => {
+  }).on('end', () => {
     var response = {
       statusCode: res.statusCode,
       headers: res.headers,
       body: resBody
     }
+    
     console.log(response)
     callback(null, response)
   })
